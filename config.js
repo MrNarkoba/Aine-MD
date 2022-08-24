39b03ffad4b1a2f1e34f65200d1042e17a156d34
MrNarkoba <111970064+MrNarkoba@users.noreply.github.com>
2022-08-25 04:52:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
   var sticker_name = '@ainee_bot'
-  var sticker_author = 'Aine'
+  var sticker_author = 'Haruka'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
