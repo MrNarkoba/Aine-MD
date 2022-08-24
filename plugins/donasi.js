87098535d35556042c3ff6ca4bb0950e2063a51a
MrNarkoba <111970064+MrNarkoba@users.noreply.github.com>
2022-08-24 22:14:47 +0700

    Add files via upload

---
+++ plugins/donasi.js
@@ -0,0 +1,11 @@
+let handler = async m => m.reply(`
+╭─「 Donasi • Dana 」
+│ •  [0878-8717-3186]]
+│ •  [0878-8717-3186]
+╰────
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
