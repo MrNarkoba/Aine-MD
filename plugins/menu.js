4f2ba3e3a65f9f5fab3e9ff8721c29e3aa97716c
MrNarkoba <111970064+MrNarkoba@users.noreply.github.com>
2022-08-26 07:54:17 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -52,7 +52,7 @@ Hai, %ucapan %name! 👋
   header: ' *%category*',
   body: ' • %cmd %islimit %isPremium',
   footer: '\n',
-  after: `*Made by ♡*
+  after: `*Made by AkmalFauzan*
 *%npmname* | %version
 ${'```%npmdesc```'}
 `,
