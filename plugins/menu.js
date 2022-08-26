292c6d26c8ae0d8337276a0bd564b9ab33a06cde
MrNarkoba <111970064+MrNarkoba@users.noreply.github.com>
2022-08-26 08:41:19 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -238,24 +238,4 @@ function ucapan() {
           ucapanWaktu = 'Selamat Malam!'
         }	
         return ucapanWaktu
-}
-let handler = async m => m.reply(`
-*RULES BOT*
--JANGAN SPAM BOT
--JANGAN TELPON/VC BOT
--JANGAN CHAT BOT/OWNER UNTUK MINTA SAVE
-*MELANGGAR?* ( *BANED PERMANEN* )
-Donasi
-  - E-money
-  •  ( Dana/OVO/Gopay )
-  - PULSA
-  •  ( Smartfren )
-  •  ( IM3 )
-  - Saweria
-  • 
-`.trim()) // Tambah sendiri kalo mau
-handler.help = ['rules']
-handler.tags = ['info']
-handler.command = /^rul(es|aturan)$/i
 
-module.exports = handle
