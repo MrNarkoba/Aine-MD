f1808da88631fe3a22fd502fcba6873ee46b4a8d
MrNarkoba <111970064+MrNarkoba@users.noreply.github.com>
2022-08-31 12:43:39 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
   let pepe = 'https://telegra.ph/file/2e2eb7d093fec08fd00d4.jpg'
   let baper = await fetch(pepe).then(a => a.buffer())
 
-  let aine = '6285693917497@s.whatsapp.net'
+  let aine = '62895403414575@s.whatsapp.net'
   let a = await conn.profilePictureUrl(conn.user.jid, 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
   let b = await conn.profilePictureUrl(owner[0]+'@s.whatsapp.net', 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
   let c = pickRandom([a, b])
@@ -24,6 +24,8 @@ Rp. 10.000 Pulsa
 Rp. 20.000 Dana
 Rp. 20.000 Pulsa
 
+4. Jadi Bot Permanen 
+Rp. 60.000 Dana
 wa.me/${owner[0]}
 *Bukan Bot!!!*
 *Owner ${conn.user.name}*
