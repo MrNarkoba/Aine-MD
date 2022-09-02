54b0c996ea0e0d84eaa936eae7020cfcf1e30560
MrNarkoba <111970064+MrNarkoba@users.noreply.github.com>
2022-09-02 14:32:34 +0700

    Update ainekawai.js

--- plugins/ainekawai.js
+++ plugins/ainekawai.js
@@ -1,10 +1,10 @@
 let fs = require('fs')
 let handler = async (m) => {
-let helloaine = fs.readFileSync('./mp3/kawai.opus')
-conn.sendFile(m.chat, helloaine, '', '', m, true)
+let kenapa = fs.readFileSync('./mp3/apa.opus')
+conn.sendFile(m.chat, kenapa, '', '', m, true)
 }
 
-handler.customPrefix = /^(kawai|kawaii)$/i
+handler.customPrefix = /^(Ppp|P|Pp|Apa|Nani?|Kenapa)$/i
 handler.command = new RegExp
 
 handler.limit = true
