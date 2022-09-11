949d5f7db8689faf9a35d13d0f0de97d04985ba5
MrNarkoba <111970064+MrNarkoba@users.noreply.github.com>
2022-09-11 11:02:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'ᴹᴿBayy-MB×፝֟͜×'
-  var sticker_author = 'Haruka'
+  var sticker_name = '@ainee_bot'
+  var sticker_author = 'Aine'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 packname = sticker_name
 author = sticker_author
-wm = '© ᴹᴿBAYY-MD×፝֟͜×
+wm = '© Aine'
 
 Intervalmsg = 1800 //detik
 
